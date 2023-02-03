c2e50969a4adb28a316e71ce25499ed6f0f8a1fc
IgorianS <62936832+IgorianS@users.noreply.github.com>
2023-02-03 20:35:50 +0300

    Update hello.py

--- web/hello.py
+++ web/hello.py
@@ -1,2 +1,7 @@
-def parse(QUERY_STRING: str) -> list:
-    return QUERY_STRING.lstrip('/?').split('&')
+def wsgi_application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    start_response(status, headers )
+
+    body = environ['QUERY_STRING'].lstrip('/?').split('&')
+    return body
