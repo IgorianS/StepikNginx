3c12506ace10492075d4730b4440e7c68221eef3
IgorianS <62936832+IgorianS@users.noreply.github.com>
2023-02-09 22:44:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
+git clone https://github.com/IgorianS/StepikNginx.git /home/box/web
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo /etc/init.d/mysql start
+mysql -uroot -e "create database web;"
+mysql -uroot -e "create user 'box'@'localhost' identified by '1234';"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+
+cd ~/web/ask
+python3 manage.py makemigrations qa
+python3 manage.py migrate
