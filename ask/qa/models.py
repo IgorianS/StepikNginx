267678276eef433828ef466ea00d9233caa842b8
IgorianS <62936832+IgorianS@users.noreply.github.com>
2023-02-09 22:59:48 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+"""
+Question - вопрос
+title - заголовок вопроса
+text - полный текст вопроса
+added_at - дата добавления вопроса
+rating - рейтинг вопроса (число)
+author - автор вопроса
+likes - список пользователей, поставивших "лайк"
+
+QuestionManager - менеджер модели Question
+new - метод возвращающий последние добавленные вопросы
+popular - метод возвращающий вопросы отсортированные по рейтингу
+
+Answer - ответ
+text - текст ответа
+added_at - дата добавления ответа
+question - вопрос, к которому относится ответ
+author - автор ответа
+
+В качестве модели пользователя - используйте django.contrib.auth.models.User  из стандартной системы авторизации Django.
+"""
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.CharField(max_length=255)
+	likes = models.ManyToManyField(User, related_name='question_like_user')
+
+	
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.OneToOneField(Question)
+	author = models.CharField(max_length=255)
+
+
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	
+	def popular(self):
+		return self.order_by('-rating')
