274115958b4db34a24cfc3c8c713687c7ab7e4ae
IgorianS <62936832+IgorianS@users.noreply.github.com>
2023-02-09 23:09:18 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/IgorianS/StepikNginx.git /home/box/web
-
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
